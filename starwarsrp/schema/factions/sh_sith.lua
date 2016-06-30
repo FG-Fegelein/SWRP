da85cfcf0787a4f416d4d4f4b0b3fe50fde4830b
BuildTools <unconfigured@null.spigotmc.org>
2016-06-30 21:58:16 +0100

    Factions - Fix 0002
    Added a new sith faction.

---
+++ starwarsrp/schema/factions/sh_sith.lua
@@ -0,0 +1,7 @@
+FACTION.name = "Sith"
+FACTION.desc = ""
+FACTION.color = Color(255, 0, 0)
+FACTION.isDefault = false
+FACTION.weapons = {"star_wars_lightsabers"}
+FACTION.pay = 450
+FACTION_SITH = FACTION.index
