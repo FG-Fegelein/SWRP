75bce3c16facee5d214bf50fcae00956b60c2035
FG-Fegelein <actuscorporation@gmail.com>
2016-06-30 18:43:04 -0300

    Factions - Fix 0005
    Fixed some things in the Citizen, Jedi and Sith, recently added to the
    gamemode.

--- starwarsrp/schema/factions/sh_citizen.lua
+++ starwarsrp/schema/factions/sh_citizen.lua
@@ -2,5 +2,5 @@ FACTION.name = "Citizen"
 FACTION.desc = ""
 FACTION.color = Color(0, 255, 0)
 FACTION.isDefault = true
-FACTION.pay = 0
+FACTION.pay = 25
 FACTION_CITIZEN = FACTION.index
--- starwarsrp/schema/factions/sh_jedi.lua
+++ starwarsrp/schema/factions/sh_jedi.lua
@@ -2,6 +2,5 @@ FACTION.name = "Jedi"
 FACTION.desc = ""
 FACTION.color = Color(0, 0, 255)
 FACTION.isDefault = false
-FACTION.weapons = {"star_wars_lightsabers"}
 FACTION.pay = 450
 FACTION_JEDI = FACTION.index
