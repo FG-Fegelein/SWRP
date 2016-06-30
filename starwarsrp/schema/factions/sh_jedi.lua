3efb607fc4d0da979d46f10dc1e12ac903290c41
BuildTools <unconfigured@null.spigotmc.org>
2016-06-30 22:15:03 +0100

    Factions fix - 0003
    Added a jedi faction

---
+++ starwarsrp/schema/factions/sh_jedi.lua
@@ -0,0 +1,7 @@
+FACTION.name = "Jedi"
+FACTION.desc = ""
+FACTION.color = Color(0, 0, 255)
+FACTION.isDefault = false
+FACTION.weapons = {"star_wars_lightsabers"}
+FACTION.pay = 450
+FACTION_JEDI = FACTION.index
